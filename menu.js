3dae25548c60425aa55f915763b3e29d883a8e74
Gabriel Santana <162528876+GabrielSan06@users.noreply.github.com>
2024-09-03 13:26:33 +0000

    finalização do responsivo, criação do menu e início das funcionalidades

---
+++ menu.js
@@ -0,0 +1,6 @@
+let btnAbrir = document.getElementById('btn-abrir')
+let menu = document.getElementById('menu-mobile')
+
+btnAbrir.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
