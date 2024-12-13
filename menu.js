9b3a95e170796123026bba936a4a0c0d0fb4a3d8
Gabriel de Santana <jgabriel.san07@gmail.com>
2024-12-13 18:00:12 -0300

    exclusão de arquivos duplicados

--- menu.js
+++
@@ -1,6 +0,0 @@
-let btnAbrir = document.getElementById('btn-abrir')
-let menu = document.getElementById('menu-mobile')
-
-btnAbrir.addEventListener('click', ()=>{
-    menu.classList.add('abrir-menu')
-})
